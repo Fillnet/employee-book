d6bf8a6840c5be13b903b409a9c61d383bea60f4
Fillnet <filipp@tenikaytis.ru>
2023-01-15 02:18:23 +0300

    Список сотрудников

--- src/main/java/me/fillnet/employeebook/model/Employee.java
+++ src/main/java/me/fillnet/employeebook/model/Employee.java
@@ -4,9 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-//@AllArgsConstructor
-//@Getter
-//@Setter
+@AllArgsConstructor
+@Getter
+@Setter
 public class Employee {
     public final String id;
     private final String firstName;
@@ -14,39 +14,39 @@ public class Employee {
     private int department;
     private int salary;
 
-    public Employee(String id, String firstName, String lastName, int department, int salary) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.department = department;
-        this.salary = salary;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public int getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(int department) {
-        this.department = department;
-    }
-
-    public int getSalary() {
-        return salary;
-    }
-
-    public void setSalary(int salary) {
-        this.salary = salary;
-    }
+//    public Employee(String id, String firstName, String lastName, int department, int salary) {
+//        this.id = id;
+//        this.firstName = firstName;
+//        this.lastName = lastName;
+//        this.department = department;
+//        this.salary = salary;
+//    }
+//
+//    public String getId() {
+//        return id;
+//    }
+//
+//    public String getFirstName() {
+//        return firstName;
+//    }
+//
+//    public String getLastName() {
+//        return lastName;
+//    }
+//
+//    public int getDepartment() {
+//        return department;
+//    }
+//
+//    public void setDepartment(int department) {
+//        this.department = department;
+//    }
+//
+//    public int getSalary() {
+//        return salary;
+//    }
+//
+//    public void setSalary(int salary) {
+//        this.salary = salary;
+//    }
 }
